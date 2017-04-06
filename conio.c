bd4dd34311b5637b69b2e8aef6d444ed789c507c
audwlsd <audwlsd7782@naver.com>
2017-04-06 12:42:06 -0700

    Refactor GNU

--- conio.c
+++ conio.c
@@ -3,58 +3,61 @@
 static struct termios old, new;
 
 /* Initialize new terminal i/o settings */
-void initTermios(int echo) 
+void initTermios(int echo)
 {
-  tcgetattr(0, &old); /* grab old terminal i/o settings */
-  new = old; /* make new settings same as old settings */
-  new.c_lflag &= ~ICANON; /* disable buffered i/o */
-  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    new = old; /* make new settings same as old settings */
+    new.c_lflag &= ~ICANON; /* disable buffered i/o */
+    new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+    tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
 }
 
 /* Restore old terminal i/o settings */
-void resetTermios(void) 
+void resetTermios(void)
 {
-  tcsetattr(0, TCSANOW, &old);
+    tcsetattr(0, TCSANOW, &old);
 }
 
 /* Read 1 character - echo defines echo mode */
-char getch_(int echo) 
+char getch_(int echo)
 {
-  char ch;
-  initTermios(echo);
-  ch = getchar();
-  if(ch == '\x1b'){
-    if(getchar()=='[') {
-      switch(getchar()){
-      case 'A':
-        ch = '8';
-        break;
-      case 'B':
-        ch = '2';
-        break;
-      case 'C':
-        ch = '6';
-        break;
-      case 'D':
-        ch = '4';
-        break;
-      }
-    }
-  }
-  resetTermios();
-  return ch;
+    char ch;
+    initTermios(echo);
+    ch = getchar();
+    if(ch == '\x1b')
+        {
+            if(getchar()=='[')
+                {
+                    switch(getchar())
+                        {
+                        case 'A':
+                            ch = '8';
+                            break;
+                        case 'B':
+                            ch = '2';
+                            break;
+                        case 'C':
+                            ch = '6';
+                            break;
+                        case 'D':
+                            ch = '4';
+                            break;
+                        }
+                }
+        }
+    resetTermios();
+    return ch;
 }
 
 /* Read 1 character without echo */
-char getch(void) 
+char getch(void)
 {
-  return getch_(0);
+    return getch_(0);
 }
 
 /* Read 1 character with echo */
-char getche(void) 
+char getche(void)
 {
-  return getch_(1);
+    return getch_(1);
 }
 
